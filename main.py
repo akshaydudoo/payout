b9a50d211d68b99ed1f0c6b719eb69764a780aab
Akshay <akshaydudoo007@gamil.com>
2024-03-23 14:39:58 +0530

    encrypt_decrypt new

--- main.py
+++ main.py
@@ -1,53 +1,38 @@
 from fastapi import FastAPI, HTTPException
 import json
+import requests
 from crypto import encrypt, decrypt
+
 app = FastAPI()
 
-
-import os
-import base64
-
-# Generate a random Secret Key
-secret_key = base64.b64encode(os.urandom(32)).decode()
-
-print("Random Secret Key:", secret_key)
-
 # Load configuration from file
+
 with open("config.json", "r") as config_file:
     config = json.load(config_file)
 
-# Define your routes using the configuration
-@app.get("/")
-def read_root():
-    return {"message": "Test credentials loaded successfully!"}
-
-@app.get("/test")
-def test_credentials():
-    return {
-        "api_endpoint_url": config["api_endpoint_url"],
-        "encryption_key": config["encryption_key"],
-        "ag_id": config["ag_id"],
-        "m_id": config["m_id"],
-        "session_id": config["session_id"],
-        "u_id": config["u_id"]
-    }
-
-# secret_key="123456"
-
+headers = {
+            "Content-Type": "application/json"
+        }
 # Example route to encrypt a payload
-@app.post("/encrypt")
-def encrypt_payload(payload: dict):
+@app.post("/encrypt_decrypt")
+def encrypt_decrypt(payload: dict):
     try:
-        encrypted_payload = encrypt(json.dumps(payload), secret_key)
-        return {"encrypted_payload": encrypted_payload}
-    except Exception as e:
-        raise HTTPException(status_code=500, detail=str(e))
+        # Encrypt the payload
+        encrypted_payload = encrypt(json.dumps(payload), config["encryption_key"])
 
-# Example route to decrypt a payload
-@app.post("/decrypt")
-def decrypt_payload(encrypted_payload: str):
-    try:
-        decrypted_payload = decrypt(encrypted_payload, secret_key)
+        # Construct the request data for decryption
+        decryption_data = {
+            "payload": encrypted_payload,
+            "uId": "AGEN5500134316"
+        }
+
+        # Send the encrypted payload to the API endpoint
+        response = requests.post(config["api_endpoint_url"], json=decryption_data, headers=headers)
+
+        # Decrypt the response payload
+        decrypted_payload = decrypt(response.json()["payload"], config["encryption_key"])
+
         return {"decrypted_payload": json.loads(decrypted_payload)}
+    
     except Exception as e:
         raise HTTPException(status_code=500, detail=str(e))
